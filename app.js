a2d0bf634e03481b1979fd78898e48858a1ff3cc
Joelymodevs <joelmortondev@gmail.com>
2023-10-20 16:51:41 +0100

    finishing vertex additions and new glitch effect pass

--- app.js
+++ app.js
@@ -12,9 +12,57 @@ import { SavePass } from 'three/examples/jsm/postprocessing/SavePass.js'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
 import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
 import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
+import { SoftGlitchPass } from './shaders/passes/SoftGlitch.js'
 
 import vertexShader from './shaders/vertex.glsl' 
 import fragmentShader from './shaders/fragment.glsl' 
+import TRACK from './sounds/noco.mp3'
+import gsap from 'gsap'
+
+class Visualizer {
+  constructor(mesh, frequencyUniformName) {
+    this.mesh = mesh;
+    this.frequencyUniformName = frequencyUniformName;
+    this.mesh.material.uniforms[this.frequencyUniformName] = {value: 0};
+
+
+    this.listener = new THREE.AudioListener()
+    this.mesh.add(this.listener)
+
+
+    this.sound = new THREE.Audio(this.listener)
+    this.loader = new THREE.AudioLoader()
+
+
+    this.analyser = new THREE.AudioAnalyser(this.sound, 32)
+  }
+
+  load(path) {
+    this.loader.load(path, (buffer) => {
+      this.sound.setBuffer(buffer)
+      this.sound.setLoop(true)
+      this.sound.setVolume(0.5)
+      this.sound.play()
+    })
+  }
+
+  getFrequency() {
+    return this.analyser.getAverageFrequency()
+    
+  }
+
+  update() {
+    const freq = Math.max(this.getFrequency() - 100, 0) / 45;
+    const freqUniform = this.mesh.material.uniforms[this.frequencyUniformName]
+    gsap.to(freqUniform, {
+      duration: 1.0,
+      ease: 'Slow.eastOut',
+      value: freq,
+    })
+  
+    return freq
+  }
+}
 
 const startApp = () => {
   const scene = useScene()
@@ -29,18 +77,6 @@ const startApp = () => {
   const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
   scene.add(dirLight, ambientLight)
 
-
-
-
-
-
-
-
-
-
-
-
-
   const geometry = new THREE.SphereGeometry(1, 100, 100)
   const material = new THREE.ShaderMaterial({
     vertexShader: vertexShader,
@@ -50,6 +86,16 @@ const startApp = () => {
     }
   })
   const ico = new THREE.Mesh(geometry, material)
+  const wireframe = new THREE.LineSegments(geometry, material)
+  const WIREFRAME_DELTA = 0.010;
+
+  wireframe.scale.setScalar(1 + WIREFRAME_DELTA)
+
+  ico.add(wireframe)
+
+  const visualizer = new Visualizer(ico, 'uAudioFrequency')
+
+  visualizer.load(TRACK)
   scene.add(ico)
 
   // GUI
@@ -81,10 +127,16 @@ const startApp = () => {
   addPass(savePass)
   addPass(outputPass)
 
+  const softGlitch = new SoftGlitchPass() 
+  addPass(softGlitch)
+
   
 
   useTick(({ timestamp, timeDiff }) => {
     material.uniforms.uTime.value = timestamp / 1000
+    const freq = visualizer.update()
+
+    softGlitch.factor = freq > 0.6 ? 0.7 : 0.2; 
   })
 }
 
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,10 +1,50 @@
 varying vec2 vUv;
 varying float vPattern;
 uniform float uTime;
+uniform float uAudioFrequency;
 
+struct Color {
+    vec3 color;
+    float position;
+};
 
-void main() {
-    gl_FragColor = vec4(vec3(vPattern ), 1);
+#define COLOR_RAMP(colors, factor, finalColor) { \
+    const int len = 4; \
+    int index = 0; \
+    for(int i = 0; i < len - 1; i++){ \
+       Color currentColor = colors[i]; \
+       bool isInBetween = currentColor.position <= factor; \
+       index = int(mix(float(index), float(i), float(isInBetween))); \
+    } \
+    Color currentColor = colors[index]; \
+    Color nextColor = colors[index + 1]; \
+    float range = nextColor.position - currentColor.position; \
+    float lerpFactor = (factor - currentColor.position) / range; \
+    finalColor = mix(currentColor.color, nextColor.color, lerpFactor); \
 }
 
+void main() {
+    float time = uTime * (1.0 + uAudioFrequency);
 
+    vec3 color;
+    
+    vec3 mainColor = mix(vec3(0.2, 0.3, 1.6), vec3(0.4, 2.0, 0.3), uAudioFrequency);
+
+    mainColor.r *= 0.9 + sin(time) / 3.2;
+    mainColor.g *= 1.1 + cos(time / 2.0) / 2.5;
+    mainColor.b *= 0.8 + cos(time / 5.0) / 4.0;
+
+    mainColor.rgb += 0.1;
+
+
+    Color[4] colors = Color[](
+        Color(vec3(1), 0.0),
+        Color(vec3(1), 0.01),
+        Color(mainColor, 0.1),
+        Color(vec3(0.01, 0.1, 0.2), 1.0)
+    );
+
+    COLOR_RAMP(colors, vPattern, color);
+
+    gl_FragColor = vec4(color, 1.0);
+}
